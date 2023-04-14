b371f04122ae1ec9d0a662a6b1720a3b80dd04e2
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-04-14 12:07:22 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -13,35 +13,70 @@ router.post('/save', helper.authenticateToken, async (req, res) => {
         const { userid, name, email, role, agentid, mobile, country_code, password, status } = req.body;
         let primary = mongoConnection.useDb(constants.DEFAULT_DB);
         let userData = await primary.model(constants.MODELS.users, userModel).findById(req.token.userid).select('-password').lean();
-        if(userData && userData.status == true && userData.role == 'admin'){
+        if(userData && userData.status == true && (userData.role == 'admin' || userData.role == 'agent')){
             if(userid && userid != '' && mongoose.Types.ObjectId.isValid(userid)){
-                let obj = {
-                    name : name,
-                    email : email,
-                    mobile : mobile,
-                    country_code : country_code,
-                    status : (status == true) ? true : false,
-                    updatedBy : mongoose.Types.ObjectId(req.token.userid)
-                };
-                await primary.model(constants.MODELS.users, userModel).findByIdAndUpdate(userid, obj);
-                return responseManager.onSuccess('User data saved successfully...', 1, res);
-            }else{
-                let obj = {
-                    name : name,
-                    email : email,
-                    mobile : mobile,
-                    country_code : country_code,
-                    role : role,
-                    status : (status == true) ? true : false,
-                    password : await helper.passwordEncryptor(password),
-                    createdBy : mongoose.Types.ObjectId(req.token.userid),
-                    updatedBy : mongoose.Types.ObjectId(req.token.userid)
-                };
-                if(agentid && agentid != '' && mongoose.Types.ObjectId.isValid(agentid) && obj.role == 'leadmanager'){
-                  obj.agentid = agentid;
+                let existinguser = await primary.model(constants.MODELS.users, userModel).findById(userid);
+                if(userData.role == 'admin'){
+                    let obj = {
+                        name : name,
+                        email : email,
+                        mobile : mobile,
+                        country_code : country_code,
+                        status : (status == true) ? true : false,
+                        updatedBy : mongoose.Types.ObjectId(req.token.userid)
+                    };
+                    await primary.model(constants.MODELS.users, userModel).findByIdAndUpdate(userid, obj);
+                    return responseManager.onSuccess('User data saved successfully...', 1, res);
+                } else if(userData.role == 'agent' && existinguser.role == 'leadmanager'){
+                    let obj = {
+                        name : name,
+                        email : email,
+                        mobile : mobile,
+                        country_code : country_code,
+                        status : (status == true) ? true : false,
+                        updatedBy : mongoose.Types.ObjectId(req.token.userid)
+                    };
+                    await primary.model(constants.MODELS.users, userModel).findByIdAndUpdate(userid, obj);
+                    return responseManager.onSuccess('User data saved successfully...', 1, res);
+                } else {
+                    return responseManager.unauthorisedRequest(res);
                 }
-                let insertedObj = await primary.model(constants.MODELS.users, userModel).create(obj);
-                return responseManager.onSuccess('User data saved successfully...', insertedObj, res);
+            }else{
+                if(userData.role == 'admin'){
+                    let obj = {
+                        name : name,
+                        email : email,
+                        mobile : mobile,
+                        country_code : country_code,
+                        role : role,
+                        status : (status == true) ? true : false,
+                        password : await helper.passwordEncryptor(password),
+                        createdBy : mongoose.Types.ObjectId(req.token.userid),
+                        updatedBy : mongoose.Types.ObjectId(req.token.userid)
+                    };
+                    if(agentid && agentid != '' && mongoose.Types.ObjectId.isValid(agentid) && obj.role == 'leadmanager'){
+                      obj.agentid = mongoose.Types.ObjectId(agentid);
+                    }
+                    let insertedObj = await primary.model(constants.MODELS.users, userModel).create(obj);
+                    return responseManager.onSuccess('User data saved successfully...', insertedObj, res);
+                } else if (userData.role == 'agent' && role == 'leadmanager'){
+                    let obj = {
+                        name : name,
+                        email : email,
+                        mobile : mobile,
+                        country_code : country_code,
+                        role : role,
+                        status : (status == true) ? true : false,
+                        agentid : mongoose.Types.ObjectId(req.token.userid),
+                        password : await helper.passwordEncryptor(password),
+                        createdBy : mongoose.Types.ObjectId(req.token.userid),
+                        updatedBy : mongoose.Types.ObjectId(req.token.userid)
+                    };
+                    let insertedObj = await primary.model(constants.MODELS.users, userModel).create(obj);
+                    return responseManager.onSuccess('User data saved successfully...', insertedObj, res);
+                } else {
+                    return responseManager.unauthorisedRequest(res);
+                }
             }
         }else{
             return responseManager.unauthorisedRequest(res);
