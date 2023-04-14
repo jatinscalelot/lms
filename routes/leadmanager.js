0c07537166fc1fff57c9fee999f0525678b5a4d2
Jatin Dhameliya <jatin.scalelot@gmail.com>
2023-04-14 17:37:26 +0530

    update

--- routes/leadmanager.js
+++ routes/leadmanager.js
@@ -16,25 +16,48 @@ router.post('/', helper.authenticateToken, async (req, res) => {
         let userData = await primary.model(constants.MODELS.users, userModel).findById(req.token.userid).select('-password').lean();
         if(userData && userData.status == true && userData.role == 'admin' || userData.role == 'leadmanager'){
             const { page, limit, search } = req.body;
-            await primary.model(constants.MODELS.users, userModel).paginate({
-                $or: [
-                    { name: { '$regex': new RegExp(search, "i") } },
-                    { email: { '$regex': new RegExp(search, "i") } },
-                    { mobile: { '$regex': new RegExp(search, "i") } }
-                ],
-                role : 'leadmanager'
-            }, {
-                page,
-                limit: parseInt(limit),
-                sort: { _id: -1 },
-                populate: { path: 'agentid', model: primary.model(constants.MODELS.users, userModel), select: "name email mobile country_code" },
-                select: 'name role email mobile country_code status createdAt updatedAt last_login_at agentid',
-                lean: true
-            }).then((leadmanagers) => {
-                return responseManager.onSuccess('Lead Manager list..', leadmanagers, res);
-            }).catch((error) => {
-                return responseManager.onError(error, res);
-            });
+            if(userData.role == 'admin'){
+                await primary.model(constants.MODELS.users, userModel).paginate({
+                    $or: [
+                        { name: { '$regex': new RegExp(search, "i") } },
+                        { email: { '$regex': new RegExp(search, "i") } },
+                        { mobile: { '$regex': new RegExp(search, "i") } }
+                    ],
+                    role : 'leadmanager'
+                }, {
+                    page,
+                    limit: parseInt(limit),
+                    sort: { _id: -1 },
+                    populate: { path: 'agentid', model: primary.model(constants.MODELS.users, userModel), select: "name email mobile country_code" },
+                    select: 'name role email mobile country_code status createdAt updatedAt last_login_at agentid',
+                    lean: true
+                }).then((leadmanagers) => {
+                    return responseManager.onSuccess('Lead Manager list..', leadmanagers, res);
+                }).catch((error) => {
+                    return responseManager.onError(error, res);
+                });
+            }else if(userData.role == 'leadmanager'){
+                await primary.model(constants.MODELS.users, userModel).paginate({
+                    $or: [
+                        { name: { '$regex': new RegExp(search, "i") } },
+                        { email: { '$regex': new RegExp(search, "i") } },
+                        { mobile: { '$regex': new RegExp(search, "i") } }
+                    ],
+                    agentid : new mongoose.Types.ObjectId(req.token.userid),
+                    role : 'leadmanager'
+                }, {
+                    page,
+                    limit: parseInt(limit),
+                    sort: { _id: -1 },
+                    populate: { path: 'agentid', model: primary.model(constants.MODELS.users, userModel), select: "name email mobile country_code" },
+                    select: 'name role email mobile country_code status createdAt updatedAt last_login_at agentid',
+                    lean: true
+                }).then((leadmanagers) => {
+                    return responseManager.onSuccess('Lead Manager list..', leadmanagers, res);
+                }).catch((error) => {
+                    return responseManager.onError(error, res);
+                });
+            }
         }else{
             return responseManager.unauthorisedRequest(res);
         }
